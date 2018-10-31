bde487bee6ffd3a14439d773b742a90ea9121fa4
Juciandro Andrade <juciandro59@gmail.com>
2018-10-30 21:23:20 -0400

    area do banner adicionado

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function openMenu() {
+	var menu = document.getElementById('menu');
+	if(menu.style.display == "none" || menu.style.display == ""){
+		menu.style.display = "block";
+	}
+}
+function closeMenu() {
+	var close = document.getElementById('menu');
+	var test = close.style.display = "none";
+}
